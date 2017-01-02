cba27d47fba96598ab9f1e7953fd3e4336ce2b15
Key Hoffman <key.hoffman@linebounce.net>
2017-01-02 02:30:48 -0600

    Initial Commit

---
+++ SAiOS-FlickrViewerLiveCoding/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SAiOS-FlickrViewerLiveCoding
+//
+//  Created by Key Hoffman on 1/2/17.
+//  Copyright © 2017 Key Hoffman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
